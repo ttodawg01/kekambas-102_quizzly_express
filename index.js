e07821723ed0d6077286760fdc4ebc87b40e4644
Brian <bstanton773@gmail.com>
2022-11-15 11:10:42 -0600

    Add connection to MongoDB with mongoose

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const path = require('path');
+const { connectDB } = require('./src/db');
+
+// Execute the connectDB function to connect to our database
+connectDB();
 
 app.set('view engine', 'ejs')
 // Update the location of the folder for the res.render to use
